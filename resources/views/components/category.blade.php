b9951749c990e4c1705c225bed40b491e216be6a
Riansyah Divano <riansyahdivano29@gmail.com>
2024-10-14 22:26:27 +0700

    terbaru

---
+++ resources/views/components/category.blade.php
@@ -0,0 +1,49 @@
+<div class="max-w-7xl mx-auto p-6">
+    <div class="mb-7">
+        <h1 class="text-3xl font-bold ml-4">Category</h1>
+    </div>
+
+    <div class="grid grid-cols-3 gap-4">
+        <!-- Card 1 -->
+        <a href="#" class="relative block overflow-hidden rounded-lg shadow-lg">
+            <img src="img/mouse.png" alt="Bubble Wrap" class="object-cover w-full h-48">
+            <div class="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2 text-sm">
+                Mouse
+            </div>
+        </a>
+        <!-- Card 2 -->
+        <a href="#" class="relative block overflow-hidden rounded-lg shadow-lg">
+            <img src="img/headset.png" alt="Kursi Gaming" class="object-cover w-full h-48">
+            <div class="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2 text-sm">
+                Headset
+            </div>
+        </a>
+        <!-- Card 3 -->
+        <a href="#" class="relative block overflow-hidden rounded-lg shadow-lg">
+            <img src="img/keyboard.png" alt="Kulkas 1 Pintu" class="object-cover w-full h-48">
+            <div class="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2 text-sm">
+                Keyboard
+            </div>
+        </a>
+        <!-- Card 4 -->
+        <a href="#" class="relative block overflow-hidden rounded-lg shadow-lg">
+            <img src="img/vga.png" alt="Pompa Air" class="object-cover w-full h-48">
+            <div class="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2 text-sm">
+                VGA
+            </div>
+        </a>
+        <!-- Card 5 -->
+        <a href="#" class="relative block overflow-hidden rounded-lg shadow-lg">
+            <img src="img/monitor.png" alt="Headset Gaming" class="object-cover w-full h-48">
+            <div class="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2 text-sm">
+                Monitor
+            </div>
+        </a>
+        <!-- Card 6 -->
+        <a href="#" class="relative block overflow-hidden rounded-lg shadow-lg">
+            <img src="img/ram.png" alt="Mechanical Keyboard" class="object-cover w-full h-48">
+            <div class="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2 text-sm">
+                RAM
+            </div>
+        </a>
+    </div>
