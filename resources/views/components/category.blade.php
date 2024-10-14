292b08a672a5e64f55744be1faa6346880698605
Riansyah Divano <riansyahdivano29@gmail.com>
2024-10-14 23:03:37 +0700

    baru stek

--- resources/views/components/category.blade.php
+++ resources/views/components/category.blade.php
@@ -7,7 +7,7 @@
         <!-- Card 1 -->
         <a href="#" class="relative block overflow-hidden rounded-lg shadow-lg">
             <img src="img/mouse.png" alt="Bubble Wrap" class="object-cover w-full h-48">
-            <div class="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2 text-sm">
+            <div class="absolute bottom-0 left-0 text-white p-2 text-sm">
                 Mouse
             </div>
         </a>
@@ -21,28 +21,28 @@
         <!-- Card 3 -->
         <a href="#" class="relative block overflow-hidden rounded-lg shadow-lg">
             <img src="img/keyboard.png" alt="Kulkas 1 Pintu" class="object-cover w-full h-48">
-            <div class="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2 text-sm">
+            <div class="absolute bottom-0 left-0  text-white p-2 text-sm">
                 Keyboard
             </div>
         </a>
         <!-- Card 4 -->
         <a href="#" class="relative block overflow-hidden rounded-lg shadow-lg">
             <img src="img/vga.png" alt="Pompa Air" class="object-cover w-full h-48">
-            <div class="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2 text-sm">
+            <div class="absolute bottom-0 left-0 text-white p-2 text-sm bg-black bg-opacity-50">
                 VGA
             </div>
         </a>
         <!-- Card 5 -->
         <a href="#" class="relative block overflow-hidden rounded-lg shadow-lg">
             <img src="img/monitor.png" alt="Headset Gaming" class="object-cover w-full h-48">
-            <div class="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2 text-sm">
+            <div class="absolute bottom-0 left-0 text-white p-2 text-sm">
                 Monitor
             </div>
         </a>
         <!-- Card 6 -->
         <a href="#" class="relative block overflow-hidden rounded-lg shadow-lg">
             <img src="img/ram.png" alt="Mechanical Keyboard" class="object-cover w-full h-48">
-            <div class="absolute bottom-0 left-0 bg-black bg-opacity-50 text-white p-2 text-sm">
+            <div class="absolute bottom-0 left-0  text-white p-2 text-sm">
                 RAM
             </div>
         </a>
